aba79beaae8d572e92feb81ebc537719d25ba58e
YoruichiWaifu <49327098+YoruichiWaifu@users.noreply.github.com>
2019-04-07 17:32:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ bot.on('message', function(message) {
 			'\n-dfpoff : [COMMANDE EN MAINTENANCE] Jeu de pile ou face. Ici, affiche le score global des joueurs et du bot [COMMANDE EN MAINTENANCE].' +
 			'\n-df6d : Lancé de dé à six faces.' +
 			'\n-df8ball : Jeu du 8ball affichant une réponse à une de vos questions pouvant être répondu par oui ou non. Exemple : df8ball Suis-je majeur ?```')
-		.then(user => console.log(`Sent user: ${user.username}`))
+		        console.log(`Sent user: ${user}`))
 	}
 
 	if (message.content === 'dfhelp2') {
